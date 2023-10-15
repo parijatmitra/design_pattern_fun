c77d4f5105aea0b41658f06cc04c791159ed3a4e
“parijatmitra” <parijatmitra20@gmail.com>
2023-10-15 10:47:14 +0530

    abstract design patter added

--- index.js
+++ index.js
@@ -69,3 +69,91 @@ console.log(burger1, 'burger 1st instance')
 //instance 2
 let burger2 = new BurgerBuilder().putCheese(false).putChilli(true).putDiscount(false).build();
 console.log(burger2, 'burger 2nd instance')
+
+//Abstract factory
+
+class GUIFactory {
+
+    createButton() {
+        throw new Error("This is an abstract method and has to be overridden");
+    }
+
+    createCheckBox() {
+        throw new Error("This is an abstract method and has to be overridden");
+    }
+
+};
+
+//Concrete Factory 1
+
+class MacFactory extends GUIFactory {
+    createButton() {
+        return new MacButton();
+    }
+    createCheckBox() {
+        return new MacCheckox();
+    }
+
+};
+
+//Concrete factory 2
+class WindowFactory extends GUIFactory {
+
+    createButton() {
+        return new WindowButton();
+    }
+
+    createCheckBox() {
+        return new WindowCheckBox();
+    }
+
+};
+
+//product A1 
+
+class MacButton {
+   paint() {
+    return "this is a mac button";
+   }
+};
+
+//product A2 
+
+class MacCheckox {
+  paint() {
+    return "this is a mac check box";
+  }
+};
+
+//product B1
+
+class WindowButton {
+  paint() {
+    return "this is a window button";
+  }
+};
+
+//product B2
+
+class WindowCheckBox {
+  paint() {
+    return "this is  a window checkbox";
+  }
+};
+
+//client code
+
+function renderUI(factory) {
+    const button = factory.createButton();
+    const checkBox = factory.createCheckBox();
+
+    console.log(button.paint());
+    console.log(checkBox.paint());
+}
+
+const macFactory = new MacFactory();
+const windowFactory = new WindowFactory();
+
+renderUI(macFactory);
+
+renderUI(windowFactory);
