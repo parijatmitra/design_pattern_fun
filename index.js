a594144845d94c1043fc2392e01e52bec9cab566
“parijatmitra” <parijatmitra20@gmail.com>
2023-10-15 10:09:47 +0530

    builder design added

--- index.js
+++ index.js
@@ -24,4 +24,48 @@ if(carCheck) {
 }
 
 
+//builder design 
 
+// product class
+class Burger {
+  constructor(builder) {
+    this.hasChesse = builder.hasCheese || false;
+    this.hasChilli = builder.hasChilli || false;
+    this.hasDiscount = builder.hasDiscount || false;;
+  }
+};
+
+
+// builder class
+class BurgerBuilder {
+      contructor() {
+
+      }
+
+      putCheese(dataEnter) {
+        this.hasCheese = dataEnter;
+        return this;
+      }
+
+      putChilli(dataEnter) {
+        this.hasChilli = dataEnter;
+        return this;
+      }
+
+      putDiscount(dataEnter) {
+        this.hasDiscount = dataEnter;
+        return this;
+      }
+
+      build() {
+        return new Burger(this);
+      }
+};
+
+// instance 1
+let burger1 = new BurgerBuilder().putCheese(true).putChilli(true).putDiscount(true).build();
+console.log(burger1, 'burger 1st instance')
+
+//instance 2
+let burger2 = new BurgerBuilder().putCheese(false).putChilli(true).putDiscount(false).build();
+console.log(burger2, 'burger 2nd instance')
